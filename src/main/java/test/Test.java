30305c9fdc5ae12f93082f48cc6acc2d19d37840
Sasha Morton-Salmon <mortonss@myumanitoba.ca>
2024-07-06 17:08:04 -0500

    update Test.java

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,7 +1,17 @@
 package test;
 
-public class Test {
-	public static void main(String[] args) {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class Test {
+	public static void main(String[] args) throws Exception {
+		backend.Transaction.readFile("SIMPLII.csv", 1, 3, 2, 4, 0);
+		// Category cat = new Category("food");
+
+		SimpleDateFormat f = new SimpleDateFormat("MM/dd/yyyy");
+		Date end = new Date();
+		Date start = f.parse("06/02/2024");
+		double expenses = backend.Transaction.getExpenses(start, end);
+		System.out.println(expenses);
 	}
 }
