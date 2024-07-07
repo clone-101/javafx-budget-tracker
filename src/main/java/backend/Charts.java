0c00374daea23a777effc8ef830af8affcc628b7
Sasha Morton-Salmon <mortonss@myumanitoba.ca>
2024-07-07 13:32:31 -0500

    add getDates method to Charts.java

--- src/main/java/backend/Charts.java
+++ src/main/java/backend/Charts.java
@@ -1,11 +1,23 @@
 package backend;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
 import javafx.scene.chart.XYChart;
 
 @SuppressWarnings({ "rawtypes", "unchecked" })
+
 public class Charts {
+	// eventually will not be hardcoded
+	private static int numMonths = 12;
+
 	// barChart
 	public static void getBarChart() {
+		// format for yAxis labels
+		SimpleDateFormat f = new SimpleDateFormat("MMM");
+
 		XYChart.Series fundsIn = new XYChart.Series();
 		fundsIn.setName("Funds In");
 
@@ -13,6 +25,29 @@ public class Charts {
 		fundsOut.setName("Funds Out");
 
 		fundsIn.getData().add(new XYChart.Data("Jan", 2000));
+
+	}
+
+	public static ArrayList<Date> getDates() {
+
+		// calendar arithmatic to get the first day of the last 12 months
+		Calendar calendar = Calendar.getInstance();
+		ArrayList<Date> months = new ArrayList<>();
+		months.add(new Date());
+
+		// set date to midnight on the first of the current month;
+		calendar.set(Calendar.DAY_OF_MONTH, 1);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+
+		for (int i = 0; i < numMonths; i++) {
+			months.add(0, calendar.getTime());
+			calendar.add(Calendar.MONTH, -1);
+		}
+
+		return months;
 
 	}
 }
--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,8 +1,5 @@
 package test;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class Test {
 	public static void main(String[] args) throws Exception {
 		backend.Transaction.readFile("D:\\Projects\\budget_tracker_fx\\src\\main\\java\\application\\SIMPLII.csv", 1, 3,
@@ -10,10 +7,12 @@ public class Test {
 		// Category cat = new Category("food");
 		// backend.Transaction.printAll();
 
-		SimpleDateFormat f = new SimpleDateFormat("MM/dd/yyyy");
-		Date end = new Date();
-		Date start = f.parse("06/02/2024");
-		double expenses = backend.Transaction.getExpenses(start, end);
-		System.out.println(expenses);
+		// SimpleDateFormat f = new SimpleDateFormat("MM/dd/yyyy");
+		// Date end = new Date();
+		// Date start = f.parse("06/02/2024");
+		// double expenses = backend.Transaction.getExpenses(start, end);
+		// System.out.println(expenses);
+
+		backend.Charts.getDates();
 	}
 }
