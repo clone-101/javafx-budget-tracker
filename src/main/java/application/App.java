c1efa1fc49ebfa8da9cc982b8b5240058d9d2711
Sasha Morton-Salmon <mortonss@myumanitoba.ca>
2024-07-07 18:08:21 -0500

    add application.css

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -15,6 +15,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("Main"));
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
