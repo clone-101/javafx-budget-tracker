b0685bb15805ec48b0b0263a7483a54a8e014e89
Sasha Morton-Salmon <mortonss@myumanitoba.ca>
2024-07-11 18:16:38 -0500

    update Category.java: implemented 2 type system for income/expenses

--- src/main/java/backend/Category.java
+++ src/main/java/backend/Category.java
@@ -4,80 +4,150 @@ import java.util.ArrayList;
 
 public class Category {
 	// ArrayList of all categories
-	private static ArrayList<Category> categories = new ArrayList<>();
-	private static ArrayList<String> names = new ArrayList<>();
-	private static final Category DEFAULT = new Category("other");
-	private static final String[] BASE_CATEGORIES = { "food", "bills", "transportation", "shopping", "income" };
+	private static ArrayList<Category> expenseCategories = new ArrayList<>();
+	private static ArrayList<Category> incomeCategories = new ArrayList<>();
+	private static ArrayList<String> expenseNames = new ArrayList<>();
+	private static ArrayList<String> incomeNames = new ArrayList<>();
+
+	// provide true for expense category, false for income category
+	private static final boolean EXPENSE = true, INCOME = false;
+
+	// default categories can be changed in settings
+	private static String[] BASE_EXPENSE_CATEGORIES = { "other", "food", "bills", "transportation", "shopping" };
+	private static String[] BASE_INCOME_CATEGORIES = { "other", "salary", "gifts", "investments", "odd jobs" };
 
 	// static methods
 
 	public static void initialize() {
-		for (String s : BASE_CATEGORIES) {
-			new Category(s);
+		for (String s : BASE_EXPENSE_CATEGORIES) {
+			new Category(s, true);
+		}
+		for (String s : BASE_INCOME_CATEGORIES) {
+			new Category(s, false);
 		}
 	}
 
-	public static ArrayList<Category> getCategories() {
-		return categories;
+	// static getters
+	public static ArrayList<Category> getExpenseCategories() {
+		return expenseCategories;
 	}
 
-	public static ArrayList<String> getNames() {
-		return names;
+	public static ArrayList<Category> getIncomeCategories() {
+		return incomeCategories;
 	}
 
-	public static void addAll(String[] names) {
-		for (String s : names) {
-			new Category(s);
+	public static ArrayList<String> getExpenseNames() {
+		return expenseNames;
+	}
+
+	public static ArrayList<String> getIncomeNames() {
+		return incomeNames;
+	}
+
+	public static void addAllIncome(String[] names) {
+		for (String s : incomeNames) {
+			new Category(s, INCOME);
+		}
+	}
+
+	public static void addAllExpense(String[] names) {
+		for (String s : expenseNames) {
+			new Category(s, EXPENSE);
 		}
 	}
 
 	// creates category if it doesn't exist
-	public static Category get(String name) {
+	public static Category get(String name, boolean type) {
 		name = name.toLowerCase().trim();
-		for (Category c : categories) {
-			if (c.name.equals(name))
-				return c;
+		if (type) { // if expense
+			for (Category c : expenseCategories) {
+				if (c.name.equals(name))
+					return c;
+			}
+		} else { // if income
+			for (Category c : incomeCategories) {
+				if (c.name.equals(name))
+					return c;
+			}
+
 		}
-		return new Category(name);
+
+		return new Category(name, type);
 	}
 
-	public static void delete(String name) {
+	public static void delete(String name, boolean type) {
+		Category temp = get(name, type);
+		Category standard = get("other", type);
 
-		Category temp = get(name);
 		// sets all transactions with this category to default
 		for (String s : temp.descriptions) {
 			for (Transaction tr : Transaction.transactions) {
 				if (tr == null)
 					break;
 				if (tr.getDescription().equals(s)) {
-					tr.setCategory(DEFAULT);
+					tr.setCategory(standard);
 				}
 			}
 		}
 		// removes category from lists
-		categories.remove(temp);
-		names.remove(name);
+		if (type) { // if expense
+			expenseCategories.remove(temp);
+			expenseNames.remove(name);
+		} else { // if income
+			incomeCategories.remove(temp);
+			incomeNames.remove(name);
+		}
+	} // delete()
+
+	// tests if a name belongs to a category (income/expense specific)
+	public static boolean isCategory(String name, boolean type) {
+		boolean isCategory = false;
+		name = name.toLowerCase().trim();
+		// type is true if expense, false if income
+		// what in JS syntax is this btw :D
+		ArrayList<Category> categories = type ? expenseCategories : incomeCategories;
+		for (Category c : categories) {
+			if (c.name.equals(name))
+				isCategory = true;
+		}
+		return isCategory;
 	}
 
 	// instance variables
 	private String name;
-	private ArrayList<String> descriptions = new ArrayList<>();
+	private boolean type; // true if expense, false if income
+	private ArrayList<String> descriptions = new ArrayList<>(); // list of all descriptions with this category
 
 	// constructor
-	public Category(String name) {
+	public Category(String name, boolean type) {
 		this.name = name.toLowerCase().trim();
-		if (!categories.contains(this)) {
-			categories.add(this);
-			names.add(name);
+		this.type = type;
+		// if type is true then it is an expense category
+		if (type) {
+			if (!expenseCategories.contains(this)) {
+				expenseCategories.add(this);
+				expenseNames.add(name);
+			}
+			// else it is an income category
+		} else {
+			if (!incomeCategories.contains(this)) {
+				incomeCategories.add(this);
+				incomeNames.add(name);
+			}
 		}
+	}
+
+	// getters
+	public boolean getType() {
+		return type;
 	}
 
 	// adds all similar transaction to the category
 	public void add(Transaction adding) {
 		String description = adding.getDescription();
 		for (Transaction tr : Transaction.transactions) {
-			if (tr == null)
-				break;
+			if (tr == null) // skip null transactions (PFA)
+				continue;
 			if (tr.getDescription().equals(description)) {
 				if (tr.getCategory() == null || !tr.getCategory().equals(this)) {
 					tr.setCategory(this);
@@ -88,23 +158,12 @@ public class Category {
 
 	// equal based on name
 	public boolean equals(Category other) {
-		return name.equals(other.name);
+		return name.equals(other.name) && type == other.type;
 	}
 
 	// :)
 	public String toString() {
 		return name;
-	}
-
-	// static methods
-	// tests if a name belongs to a category
-	public static boolean isCategory(String name) {
-		name = name.toLowerCase().trim();
-		for (Category c : categories) {
-			if (c.name.equals(name))
-				return true;
-		}
-		return false;
 	}
 
 }
