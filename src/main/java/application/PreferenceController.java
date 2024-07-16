923400600fd4a26a487f2613e2fa77b9a7a412af
Sasha Morton-Salmon <mortonss@myumanitoba.ca>
2024-07-15 22:36:12 -0500

    add search to Preferences

--- src/main/java/application/PreferenceController.java
+++ src/main/java/application/PreferenceController.java
@@ -2,6 +2,7 @@ package application;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import backend.Category;
@@ -11,9 +12,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.KeyCode;
 
 public class PreferenceController implements Initializable {
 	// final variables for category type
@@ -26,13 +29,16 @@ public class PreferenceController implements Initializable {
 	ToggleGroup categoryType;
 
 	@FXML // textField for creating new category
-	TextField createCategoryField;
+	TextField createCategoryField, searchTrList;
 
 	@FXML
-	ComboBox<String> bulkAssignDescription, bulkAssignCategory, deleteCategory;
+	ComboBox<String> bulkAssignDescription, bulkAssignCategory, deleteCategory, trListBox;
 
 	@FXML
-	Button createCategoryBtn, deleteCategoryBtn, bulkAssignBtn;
+	ListView<Transaction> trList;
+
+	@FXML
+	Button createCategoryBtn, deleteCategoryBtn, bulkAssignBtn, trListBtn;
 
 	@FXML // switch to application main page
 	private void switchToMain() throws IOException {
@@ -59,6 +65,11 @@ public class PreferenceController implements Initializable {
 
 	@FXML
 	private void handleDeleteCategory(ActionEvent event) {
+		if (deleteCategory.getValue() == null || deleteCategory.getValue().equals("other")
+				|| deleteCategory.getValue().length() == 0) {
+			deleteCategory.setStyle("-fx-border-color: red ; -fx-border-width: 2px ;");
+			return;
+		}
 		Category deleting = Category.get(deleteCategory.getValue(), expense.isSelected());
 		Category other = Category.get("other", expense.isSelected());
 
@@ -74,8 +85,8 @@ public class PreferenceController implements Initializable {
 	@FXML
 	private void handleBulkAssign(ActionEvent event) {
 		if (bulkAssignCategory.getValue() == null || bulkAssignDescription.getValue() == null
-				|| bulkAssignDescription.getValue().length() == 0) {
-			if (bulkAssignCategory.getValue() == null) {
+				|| bulkAssignDescription.getValue().length() == 0 || bulkAssignCategory.getValue().length() == 0) {
+			if (bulkAssignCategory.getValue() == null || bulkAssignCategory.getValue().length() == 0) {
 				bulkAssignCategory.setStyle("-fx-border-color: red ; -fx-border-width: 2px ;");
 			}
 			if (bulkAssignDescription.getValue() == null || bulkAssignDescription.getValue().length() == 0) {
@@ -96,6 +107,36 @@ public class PreferenceController implements Initializable {
 		bulkAssignDescription.setStyle("");
 
 	} // handleBulkAssign()
+
+	@FXML
+	private void handleListView(ActionEvent event) {
+		// Transaction tr = trList.getSelectionModel().getSelectedItem();
+		// if (tr != null && trListBox.getValue() != null) {
+		// Category category = Category.get(trListBox.getValue(), expense.isSelected());
+		// Category.bulkReassign(tr.getDescription(), category, expense.isSelected());
+		// Transaction[] list = Transaction.getTransactions(expense.isSelected(),
+		// searchTrList.getText());
+		// trList.getItems().clear();
+		// trList.getItems().addAll(list);
+		// trList.refresh();
+
+		// }
+
+		// allows for multiple selection
+		List<Transaction> selectedTransactions = trList.getSelectionModel().getSelectedItems();
+		if (!selectedTransactions.isEmpty() && trListBox.getValue() != null) {
+			for (Transaction tr : selectedTransactions) {
+				if (tr != null) {
+					Category category = Category.get(trListBox.getValue(), expense.isSelected());
+					Category.bulkReassign(tr.getDescription(), category, expense.isSelected());
+				}
+			}
+			Transaction[] list = Transaction.getTransactions(expense.isSelected(), searchTrList.getText());
+			trList.getItems().clear();
+			trList.getItems().addAll(list);
+			trList.refresh();
+		}
+	} // handleListView()
 
 	// ************* csv variables *************
 
@@ -159,6 +200,7 @@ public class PreferenceController implements Initializable {
 		initializeCreateCategory();
 		initializeDeleteCategory();
 		initializeBulkAssign();
+		initializeListView();
 
 		// csv
 		initializeCSVPreferences();
@@ -167,30 +209,59 @@ public class PreferenceController implements Initializable {
 
 	// used to refresh all fields after an action
 	private void refresh() {
-		// refresh category preferences
+		refreshCreateCategory();
+		refreshDeleteCategory();
+		refreshBulkAssign();
+		refreshListView();
+		refreshCSV();
+
+	}
+
+	private void refreshCreateCategory() {
+		createCategoryField.clear();
+		createCategoryField.setStyle("");
+	}
+
+	private void refreshDeleteCategory() {
 		deleteCategory.getItems().clear();
 		deleteCategory.getItems().addAll(Category.getCategoryNames(expense.isSelected()));
-		createCategoryField.clear();
+		deleteCategory.setStyle("");
+
+	}
+
+	private void refreshBulkAssign() {
 		bulkAssignCategory.getItems().clear();
 		bulkAssignCategory.getItems().addAll(Category.getCategoryNames(expense.isSelected()));
 		bulkAssignDescription.setValue(null);
 		bulkAssignCategory.setValue(null);
 
-		// refresh csv preferences
+		bulkAssignDescription.setStyle("");
+		bulkAssignCategory.setStyle("");
+
+	}
+
+	private void refreshListView() {
+		Transaction[] list = Transaction.getTransactions(expense.isSelected(), searchTrList.getText());
+		trList.getItems().clear();
+		trList.getItems().addAll(list);
+		trList.refresh();
+
+		trListBox.getItems().clear();
+		trListBox.getItems().addAll(Category.getCategoryNames(expense.isSelected()));
+	}
+
+	private void refreshCSV() {
 		csvBox1.setValue(null);
 		csvBox2.setValue(null);
 		csvBox3.setValue(null);
 		csvBox4.setValue(null);
 
-		// reset any red borders
-		createCategoryField.setStyle("");
-		deleteCategory.setStyle("");
-		bulkAssignDescription.setStyle("");
-		bulkAssignCategory.setStyle("");
+		// styles
 		csvBox1.setStyle("");
 		csvBox2.setStyle("");
 		csvBox3.setStyle("");
 		csvBox4.setStyle("");
+
 	}
 
 	// ************* initialization methods *************
@@ -204,6 +275,7 @@ public class PreferenceController implements Initializable {
 			deleteCategory.getItems().addAll(Category.getCategoryNames(EXPENSE));
 			bulkAssignCategory.getItems().addAll(Category.getCategoryNames(EXPENSE));
 			bulkAssignDescription.getItems().addAll(Category.getDescriptions(EXPENSE));
+			refreshListView();
 		});
 		income.setOnAction(event -> {
 			deleteCategory.getItems().clear();
@@ -212,7 +284,9 @@ public class PreferenceController implements Initializable {
 			deleteCategory.getItems().addAll(Category.getCategoryNames(INCOME));
 			bulkAssignCategory.getItems().addAll(Category.getCategoryNames(INCOME));
 			bulkAssignDescription.getItems().addAll(Category.getDescriptions(INCOME));
+			refreshListView();
 		});
+
 	} // initializeChooseCategory()
 
 	private void initializeCreateCategory() {
@@ -268,6 +342,39 @@ public class PreferenceController implements Initializable {
 			}
 		});
 	} // initializeBulkAssign()
+
+	private void initializeListView() {
+		refreshListView();
+		trList.setOnKeyPressed(event -> {
+			if (event.getCode() == KeyCode.BACK_SPACE) {
+				Transaction tr = trList.getSelectionModel().getSelectedItem();
+				if (tr != null) {
+					tr.delete(tr);
+					refreshListView();
+					Transaction.saveToFile();
+					if (!trList.getItems().isEmpty())
+						trList.getSelectionModel().selectFirst();
+				}
+			} else if (event.getCode() == KeyCode.ENTER) {
+				trListBox.setValue(trListBox.getValue());
+				trListBtn.fire();
+			}
+		});
+
+		// Category comboBox
+		trListBox.getItems().clear();
+		trListBox.getItems().addAll(Category.getCategoryNames(expense.isSelected()));
+		trListBox.setOnKeyPressed(event -> {
+			if (event.getCode() == KeyCode.ENTER) {
+				trListBtn.fire();
+			}
+		});
+
+		searchTrList.textProperty().addListener((observable, oldValue, newValue) -> {
+			trList.getItems().clear();
+			trList.getItems().addAll(Transaction.getTransactions(expense.isSelected(), newValue));
+		});
+	} // initializeListView()
 
 	private void initializeCSVPreferences() {
 		String[] csvOrder = Transaction.getCSVOrderStr();
